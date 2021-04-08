2fab3975f31b2cd8b508b0c90363c5e41b26a97f
satin000 <70702756+satin000@users.noreply.github.com>
2021-04-08 22:28:35 +0200

    Изменены добавленные блоки со слайдерами

--- js/main.js
+++ js/main.js
@@ -13,3 +13,37 @@ const swiper = new Swiper('.channel-slider', {
   
    
   });
+
+  
+  const swiper2 = new Swiper('.channel-2-slider', {
+    // Optional parameters
+    
+    loop: true,
+    slidesPerView: 3,  
+   
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.channel-2-button-next',
+      prevEl: '.channel-2-button-prev',
+    },
+  
+   
+  });
+
+  const swiper3 = new Swiper('.channel-3-slider', {
+    // Optional parameters
+    
+    loop: true,
+    slidesPerView: 6,  
+   
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.channel-3-button-next',
+      prevEl: '.channel-3-button-prev',
+    },
+  
+   
+  });
+
