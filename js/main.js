358934295b8f93811fd4bb0bf2251dc8030691db
satin000 <70702756+satin000@users.noreply.github.com>
2021-04-09 23:04:05 +0200

    Добавлены правила для адаптива

--- js/main.js
+++ js/main.js
@@ -2,9 +2,27 @@ const swiper = new Swiper('.channel-slider', {
     // Optional parameters
     
     loop: true,
-    slidesPerView: 6,  
-   
-  
+    slidesPerView: 1, 
+    spaceBetween: 20,
+    breakpoints: {
+      1900: {
+        slidesPerView: 6
+      },
+      1600: {
+        slidesPerView: 5
+      },
+      1300: {
+        slidesPerView: 4
+      },
+      1100: {
+        slidesPerView: 3
+      },
+      800: {
+        slidesPerView: 2
+      }
+    },
+    
+     
     // Navigation arrows
     navigation: {
       nextEl: '.channel-button-next',
@@ -19,7 +37,18 @@ const swiper = new Swiper('.channel-slider', {
     // Optional parameters
     
     loop: true,
-    slidesPerView: 3,  
+    slidesPerView: 1,  
+    spaceBetween: 20,
+    breakpoints: {
+      1600: {
+        slidesPerView: 3
+      },
+      1100: {
+        slidesPerView: 2
+      },
+      
+    },
+    
    
   
     // Navigation arrows
@@ -35,8 +64,25 @@ const swiper = new Swiper('.channel-slider', {
     // Optional parameters
     
     loop: true,
-    slidesPerView: 6,  
-   
+    slidesPerView: 1, 
+    spaceBetween: 20,
+    breakpoints: {
+      1900: {
+        slidesPerView: 6
+      },
+      1600: {
+        slidesPerView: 5
+      },
+      1300: {
+        slidesPerView: 4
+      },
+      1100: {
+        slidesPerView: 3
+      },
+      800: {
+        slidesPerView: 2
+      }
+    },
   
     // Navigation arrows
     navigation: {
@@ -47,3 +93,18 @@ const swiper = new Swiper('.channel-slider', {
    
   });
 
+const searchBtn = document.querySelector('.mobile-search');
+const mobileSearch = document.querySelector('.input-group');
+
+searchBtn.addEventListener('click', () => {
+  mobileSearch.classList.toggle('is-open');
+
+
+});
+
+
+if(document.documentElement.scrollWidth <= 640) {
+  swiper.destroy();
+  swiper2.destroy();
+  swiper3.destroy();
+}
